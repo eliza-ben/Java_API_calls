99fe297df72a71e58ad804312e92c8a373486290
eliza-ben <elizabennette15@gmail.com>
2026-02-06 11:44:32 -0500

    Update WebClientConfig.java

--- src/main/java/com/example/uploadproxy/config/WebClientConfig.java
+++ src/main/java/com/example/uploadproxy/config/WebClientConfig.java
@@ -1,1 +1,15 @@
+package com.example.uploadproxy.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+  @Bean
+  public WebClient.Builder webClientBuilder() {
+    return WebClient.builder();
+  }
+}
 
